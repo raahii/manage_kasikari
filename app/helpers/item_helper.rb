a7242467d56e49d32df07a2e4a41becf6c5eb3f9
piyo56 <piyo56.net@gmail.com>
2017-02-28 20:57:55 +0900

    show placeholder if item has no image

--- app/helpers/item_helper.rb
+++ app/helpers/item_helper.rb
@@ -1,7 +1,8 @@
 module ItemHelper
   def thumbnail_for(item, options={ size: 80 })
     size = options[:size]
-    image_tag item.image.to_s, 
+    image_url = item.image.present? ? item.image.to_s : "http://placehold.it/80x80"
+    image_tag image_url, 
               alt: item.name , class: "gravatar", 
               width: size, height: size
   end
