9defbe1a374683c30e2b41d2d36abefd39ee1c04
piyo56 <piyo56.net@gmail.com>
2017-02-28 17:16:59 +0900

    add relationship between user and items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228061035) do
+ActiveRecord::Schema.define(version: 20170228081456) do
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                      null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170228061035) do
     t.string   "image"
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_items_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -29,4 +31,5 @@ ActiveRecord::Schema.define(version: 20170228061035) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
+  add_foreign_key "items", "users"
 end
