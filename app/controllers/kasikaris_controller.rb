f435ec5c89dcf68430a9e0cabec1c2a8af97f0b9
piyo56 <piyo56.net@gmail.com>
2017-03-03 15:22:09 +0900

    add create action

--- app/controllers/kasikaris_controller.rb
+++ app/controllers/kasikaris_controller.rb
@@ -15,6 +15,18 @@ class KasikarisController < ApplicationController
   end
 
   def create
+    # * 誰かのアイテムを借りようとしている場合は申請をして相手に承認され
+    #   るまでなけれ貸し借りとしては成立しない
+    # これ後回しかな…
+
+    @kasikari = Kasikari.new(kasikari_params)
+
+    if @kasikari.save
+      flash[:success] = "貸し借りを登録しました"
+      redirect_to @kasikari
+    else
+      redirect_to new_kasikari_path
+    end
   end
 
   def edit
@@ -32,7 +44,7 @@ class KasikarisController < ApplicationController
     @kasikari = Kasikari.find(params[:id])
   end
 
-  def kasikar_params
+  def kasikari_params
     params.require(:kasikari).permit(
       :item_id,
       :from_user_id,
@@ -41,5 +53,23 @@ class KasikarisController < ApplicationController
       :end_date,
       :done_flag
     )
+  end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    friends   = current_user.friends
+    from_user = @kasikari.from_user
+    to_user   = @kasikari.to_user
+
+    if ![from_user, to_user].include?(current_user)
+      flash[:danger] = "貸し借りにあなたが含まれていません"
+      redirect_to new_kasikari_path
+    elsif from_user == current_user && to_user == current_user
+      flash[:danger] = "貸し借りをするユーザーの組み合わせが無効です"
+      redirect_to new_kasikari_path
+    elsif friends.include?(from_user) || friends.include?(to_user)
+      flash[:danger] = "友達でないユーザーとの貸し借りはできません"
+      redirect_to new_kasikari_path
+    end
   end
 end
--- app/models/kasikari.rb
+++ app/models/kasikari.rb
@@ -15,16 +15,35 @@
 
 class Kasikari < ApplicationRecord
 
-  validates :item_id,      presence: true
-  validates :from_user_id, presence: true
-  validates :to_user_id,   presence: true
+  validates :item_id,      presence: true, allow_nil: true
+  validates :from_user_id, presence: true, allow_nil: true
+  validates :to_user_id,   presence: true, allow_nil: true
   validates :start_date,   presence: true
   validates :end_date,     presence: true
+  validate :exist_item?, :exist_from_user?, :exist_to_user?
 
   default_scope -> { order(created_at: :desc) }
 
   def init
     self.done_flag  ||= false  # will set the default value only if it's nil
+  end
+
+  def exist_item?
+    if Item.find_by(id: item_id).nil?
+      errors.add(:item_id, "無効なアイテムです")
+    end
+  end
+
+  def exist_from_user?
+    if User.find_by(id: from_user_id).nil?
+      errors.add(:from_user_id, "貸し手のユーザーが無効です")
+    end
+  end
+
+  def exist_to_user?
+    if User.find_by(id: to_user_id).nil?
+      errors.add(:to_user_id, "借り手のユーザーが無効です")
+    end
   end
 
   # 設計が良くない
@@ -40,4 +59,5 @@ class Kasikari < ApplicationRecord
   def to_user
     User.find_by(id: self.to_user_id)
   end
+  
 end
