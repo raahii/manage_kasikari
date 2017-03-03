3994c6d7b2df225ad8f49bdbfdd306a41a99f993
TaigaMikami <mikamimikami60@gmail.com>
2017-03-03 22:25:00 +0900

    complete divide scss

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # assets/stylesheets配下のファイルもassets precompile対象に
+  config.assets.precompile += ['*.scss']
 end
