2464aa880ce71c126d80e63bfbe3f1b991c466d5
piyo56 <piyo56.net@gmail.com>
2017-03-05 11:10:44 +0900

    develped webpush notification successfully

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -7,6 +7,8 @@ class SubscriptionsController < ApplicationController
   end
 
   def destroy
+    session[:subscription] = nil
+    head :ok
   end
 
   def push
@@ -19,13 +21,23 @@ class SubscriptionsController < ApplicationController
   def webpush_params
     subscription_params = fetch_subscription
 
-    message = "Hello world, the time is #{Time.zone.now}"
+    message  = "Hello world, the time is #{Time.zone.now}"
     endpoint = subscription_params["endpoint"]
-    p256dh = subscription_params.dig("keys", "p256dh")
-    auth = subscription_params.dig("keys", "auth")
-    api_key = "AIzaSyBBV9g94Btu5D7xLJkukcVWroz8sEYfiXo" if endpoint =~ /\.google.com\//
+    p256dh   = subscription_params.dig("keys", "p256dh")
+    auth     = subscription_params.dig("keys", "auth")
+    vapid = {
+      subject:     "piyo56.net@gmail.com",
+      public_key:  "BK6_74HsZx1k6i8mquyKG-dKOtgV006ULQdXuz6RYeNcURS9o7EVh8wWgQI3212qAldps3tqQYOZhpCUzFj9z5k=",
+      private_key: "3kxDPfB1QQ8gq5XKrr6SmFQcYY8hkzs4ONYvuqNPbq0=",
+    }
 
-    { message: message, endpoint: endpoint, p256dh: p256dh, auth: auth, api_key: api_key }
+    {
+      message: message,
+      endpoint: endpoint,
+      p256dh: p256dh,
+      auth: auth,
+      vapid: vapid,
+    }
   end
 
   def fetch_subscription
